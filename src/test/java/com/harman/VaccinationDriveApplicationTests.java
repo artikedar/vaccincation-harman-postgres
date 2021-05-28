17d53c44433c4c0079fab3c877e22a63e2eec893
port2pk <port2pk@gmail.com>
2021-05-28 16:18:48 +0530

    structure
    first commit

---
+++ src/test/java/com/harman/VaccinationDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

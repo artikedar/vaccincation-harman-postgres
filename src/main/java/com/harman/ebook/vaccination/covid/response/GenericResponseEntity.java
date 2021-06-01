479c648eeaa6bdd90531d5677bbd253818911c94
paam9010 <amarnath.pant.consultant@nielsen.com>
2021-06-01 10:42:08 +0530

    Updated for lombok

--- src/main/java/com/harman/ebook/vaccination/covid/response/GenericResponseEntity.java
+++ src/main/java/com/harman/ebook/vaccination/covid/response/GenericResponseEntity.java
@@ -2,6 +2,7 @@ package com.harman.ebook.vaccination.covid.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,42 +10,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class GenericResponseEntity {
 	
-	Object data;
-	Status status;
-	
-	
-	
-	public GenericResponseEntity() {}
-
-	public GenericResponseEntity(Object data, Long count, Status status) {
-		this.data = data;
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return "GenericResponseEntity [data=" + data + ", status=" + status + "]";
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	public Status getStatus() {
-		return status;
-	}
-
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-	
-	
+	private Object data;
+	private Status status;
 
 }
--- src/main/java/com/harman/ebook/vaccination/covid/response/Status.java
+++ src/main/java/com/harman/ebook/vaccination/covid/response/Status.java
@@ -1,44 +1,15 @@
 package com.harman.ebook.vaccination.covid.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-//@Getter
-//@Setter
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Status {
-	String statusFlag;
-	String statusMsg;
-	
-	public Status() {}
-
-	public Status(String statusFlag, String statusMsg) {
-		super();
-		this.statusFlag = statusFlag;
-		this.statusMsg = statusMsg;
-	}
-
-	@Override
-	public String toString() {
-		return "Status [statusFlag=" + statusFlag + ", statusMsg=" + statusMsg + "]";
-	}
-
-	public String getStatusFlag() {
-		return statusFlag;
-	}
-
-	public void setStatusFlag(String statusFlag) {
-		this.statusFlag = statusFlag;
-	}
-
-	public String getStatusMsg() {
-		return statusMsg;
-	}
-
-	public void setStatusMsg(String statusMsg) {
-		this.statusMsg = statusMsg;
-	}
-	
-	
+	private String statusFlag;
+	private String statusMsg;
 
 }

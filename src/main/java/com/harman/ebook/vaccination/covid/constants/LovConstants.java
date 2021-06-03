108ab7da29a316d9985e0c1f8a41b18c144c5b50
robh9001 <Bhagwat.Rokade.consultant@nielsen.com>
2021-06-03 20:00:38 +0530

    Lov Constants

---
+++ src/main/java/com/harman/ebook/vaccination/covid/constants/LovConstants.java
@@ -0,0 +1,19 @@
+package com.harman.ebook.vaccination.covid.constants;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Bhagwat Rokade
+ * @project HarmanVaccination
+ * @created 5/30/2021
+ */
+@Getter
+@NoArgsConstructor
+public final class LovConstants {
+  public static final int LOV_APP_STATUS_BOOKED = 1;
+  public static final int LOV_APP_STATUS_CANCELED = 2;
+  public static final int LOV_APP_STATUS_RESCHEDULED = 3;
+  public static final int LOV_APP_STATUS_COMPLETED = 4;
+}

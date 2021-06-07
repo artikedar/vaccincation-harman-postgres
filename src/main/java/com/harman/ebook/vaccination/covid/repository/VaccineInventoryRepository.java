fc659986b11c52e0c05eb642339ef6a471661c70
paam9010 <amarnath.pant.consultant@nielsen.com>
2021-06-07 14:11:56 +0530

    Query using string date for VaccineInventory for availability

--- src/main/java/com/harman/ebook/vaccination/covid/repository/VaccineInventoryRepository.java
+++ src/main/java/com/harman/ebook/vaccination/covid/repository/VaccineInventoryRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.harman.ebook.vaccination.covid.entity.VaccineInventory;
@@ -16,4 +17,10 @@ public interface VaccineInventoryRepository extends JpaRepository<VaccineInvento
 	List<VaccineInventory> findByLocationAndDateOfAvailabilityBetweenAndIsActive(Short location,Date fromDate,Date tillDate, Boolean isActive);
 
 	VaccineInventory findVaccineInventoryByVacTypeAndLocationAndDateOfAvailability(Short vacType, Short location, Date dateOfAvailability);
+
+	@Query(value = 	"SELECT vacInv FROM VaccineInventory vacInv WHERE vacInv.location = :location AND " +
+					"vacInv.dateOfAvailability <= TO_DATE(:dateOfAvailability, 'yyyy-MM-dd') and vacInv.vacType =:vacType")
+	VaccineInventory getVaccineInventoryByVacTypeLocationDOAvailability(@Param("vacType") Short vacType,
+																		@Param("location") Short location,
+																		@Param("dateOfAvailability") String dateOfAvailability);
 }

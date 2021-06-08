8e54a72925f81f85e744066e2e1a0e4d91bd9cfe
robh9001 <Bhagwat.Rokade.consultant@nielsen.com>
2021-06-08 15:44:21 +0530

    Latest Jar

--- src/main/java/com/harman/ebook/vaccination/covid/controllers/AuthController.java
+++ src/main/java/com/harman/ebook/vaccination/covid/controllers/AuthController.java
@@ -5,11 +5,13 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,11 @@ public class AuthController {
 			@PathVariable (name="empId",required = true)Integer empId,
 			@RequestParam(name="doj",required = true) String doj)throws Exception {
 		return  authService.validateUser(empId,doj);
+	}
+
+	@GetMapping("/api/auth/logout")
+	public String destroySession(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return "redirect:/";
 	}
 }

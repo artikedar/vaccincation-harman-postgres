16f8830c0b7d657898b5cdbca0ea4866f03b6ce9
paam9010 <amarnath.pant.consultant@nielsen.com>
2021-06-15 11:51:45 +0530

    Making Entities Audit aware for created on and modified on

---
+++ src/main/java/com/harman/ebook/vaccination/covid/PersistenceConfig.java
@@ -0,0 +1,15 @@
+package com.harman.ebook.vaccination.covid;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * vaccination-drive
+ *
+ * @author Amarnath Pant
+ * Created on 6/15/2021
+ */
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+}
